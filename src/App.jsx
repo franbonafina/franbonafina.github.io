7f63b285fabaf6e1c42edcff1126cd7b3c1799ea
Franco Bonafina <franbonafina1@gmail.com>
2026-03-17 13:48:28 -0300

    Apply right redirection

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useNavigate } from 'react-router-dom'
 import { Helmet } from 'react-helmet-async'
+import { useEffect } from 'react'
 import Home from './pages/Home'
 import Blog from './pages/Blog'
 import Post from './pages/Post'
 import NotFound from './pages/NotFound'
 
 function App() {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    // Handle spa-github-pages redirect
+    const query = window.location.search
+    if (query.includes('?/')) {
+      const path = query.replace('?', '')
+      navigate(path, { replace: true })
+    }
+  }, [navigate])
+
   return (
     <>
       <Routes>
